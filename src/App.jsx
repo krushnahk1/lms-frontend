3b684e68c5fb24cbed15fa3f30ecc227f71b6e29
maheshd09 <maheshdarkunde9@gmail.com>
2025-02-26 22:41:31 +0530

    Home page and About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import TeacherList from './components/TeacherList'
 import Header from './components/Header'
-
+import Home from './components/Home'
+import AboutUs from './components/AboutUs'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Header/>
+    <Home></Home>
+    <AboutUs></AboutUs>
   <TeacherList></TeacherList>
+  
+
     </>
   )
 }
